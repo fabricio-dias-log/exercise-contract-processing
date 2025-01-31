9d597965a83c6aaa75872baf5810de6ef6efebcd
Fabricio Dias <fabricio270909@gmail.com>
2025-01-30 23:52:03 -0300

    Function to process contract

--- Services/PaymentService.cs
+++ Services/PaymentService.cs
@@ -11,4 +11,15 @@ public class PaymentService
         _taxService = taxService;
     }
 
+    public void ProcessingContract(Contract contract, int installments)
+    {
+        for (int i = 1; i <= installments; i++)
+        {
+            DateOnly installmentDate = contract.Date.AddMonths(i);
+            double installmentValue = _taxService.Tax(contract.Value, i, installments);
+
+            Installments installment = new Installments(installmentValue, installmentDate);
+            contract.InstallmentsList.Add(installment);
+        }
+    }
 }
