54060265947f215e9e3af870d90b27be4b0d8557
Fabricio Dias <fabricio270909@gmail.com>
2025-01-30 23:50:16 -0300

    Calculating value with tax

--- Services/TaxService.cs
+++ Services/TaxService.cs
@@ -1,6 +1,13 @@
 ﻿namespace ExerciseContractProcessing.Services;
 
-public class TaxService
+public class TaxService : ITaxService
 {
-    
+    public double Tax(double amount, int currentInstallment,int installment)
+    {
+        double totalValue = amount /installment;
+        totalValue += (totalValue * 0.01) * currentInstallment;
+        totalValue += totalValue * 0.02;
+
+        return totalValue;
+    }
 }
