e5812d5e1c576ad6735c9495007f881cdaad6987
Fabricio Dias <fabricio270909@gmail.com>
2025-01-30 23:51:02 -0300

    Entity structured

--- Services/PaymentService.cs
+++ Services/PaymentService.cs
@@ -1,6 +1,14 @@
-﻿namespace ExerciseContractProcessing.Services;
+﻿using ExerciseContractProcessing.Entities;
+
+namespace ExerciseContractProcessing.Services;
 
 public class PaymentService
 {
-    
+    private ITaxService _taxService;
+
+    public PaymentService(ITaxService taxService)
+    {
+        _taxService = taxService;
+    }
+
 }
